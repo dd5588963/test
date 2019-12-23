164724121479b1ce8ac8e696c04ed43f37855a79
dd5588963 <741477994@qq.com>
2019-12-23 15:22:42 +0800

    添加项目文件。

---
+++ prim-.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+class X{
+public:
+	int x;
+	int y;
+	X(int m, int n);
+};
+int main()
+{
+	X a(3,2);
+	X b = a;
+	cout << b.x << b.y;
+	
+	return 0;
+}
